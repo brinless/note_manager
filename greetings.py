a8c666f720c8693cc0221f24eabd7a265a6f4137
Burnyashev <brinless@ya.ru>
2025-01-20 10:12:37 +0400

    11st commit

--- greetings.py
+++ greetings.py
@@ -1,9 +1,12 @@
-import datetime
+from datetime import datetime
+#import datetime
 
 username = input('Введите имя пользователя: ')
 title = input('Заголовок заметки: ')
 content = input('Текст заметки: ')
 status = "выполняется"
-created_date = datetime.datetime.now()
-issue_date = input('Укажите дату выполнения: ')
+created_date = datetime.now()
+issue_date = input('Укажите дату выполнения в формате: dd-mm-yyyy: ')
+issue_date = datetime.strptime(issue_date, "%d/%m/%Y")
 print(created_date.date())
+print(issue_date)
